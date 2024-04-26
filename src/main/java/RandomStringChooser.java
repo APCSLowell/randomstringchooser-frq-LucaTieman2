71adb6f73eb713c4b7035fc289deb3452a2b16b9
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-04-25 21:18:45 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,13 @@
 import java.util.*;
 public class RandomStringChooser
 {
+  private 
   public RandomStringChooser () {
   
+  }
+
+  public String getNext () {
+    
   }
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
